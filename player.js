7b59f91f3a6eb71ac39f5166e5c4618780a175b4
Michael Counts <mikecounts58@gmail.com>
2018-09-12 12:59:55 -0400

    Update player.js

--- player.js
+++ player.js
@@ -22,12 +22,12 @@ Player.prototype.undraw = function() {
 Player.prototype.moveUp = function() {
     let contact = this.detectContact(0,-1);
     console.log(contact);
-    if (contact[0] == 'wall') {
+    if (contact[0] == 'w') {
         this.undraw();
         this.y -= SQR;
         this.draw();
      }
-     else if (contact[0] == 'door') {
+     else if (contact[0] == 'd') {
          player.throughDoor();
      }
 };
@@ -35,12 +35,12 @@ Player.prototype.moveUp = function() {
 Player.prototype.moveDown = function() {
     // Need to check to srqs below since player is 2 sqrs tall
     let contact = this.detectContact(0,2);
-    if (contact[0] == 'wall') {
+    if (contact[0] == 'w') {
         this.undraw();
         this.y += SQR;
         this.draw();
     }
-    else if (contact[0] == 'door') {
+    else if (contact[0] == 'd') {
          player.throughDoor();
     }
 };
@@ -49,15 +49,15 @@ Player.prototype.moveLeft = function() {
     //need to check sqrs form top and bottom sqrs of player
     let top_contact = this.detectContact(-1,0);
     let bot_contact = this.detectContact(-1,1);
-    if (top_contact[0] == 'wall' && bot_contact[0] == 'wall') {
+    if (top_contact[0] == 'w' && bot_contact[0] == 'w') {
         this.undraw();
         this.x -= SQR;
         this.draw();
     }
-    else if (top_contact[0] == 'door') {
+    else if (top_contact[0] == 'd') {
         player.throughDoor(top_contact[1], top_contact[2]);
     }
-    else if (bot_contact[0] == 'door') {
+    else if (bot_contact[0] == 'd') {
         player.throughDoor(bot_contact[1], bot_contact[2]);
     }
 };
@@ -66,15 +66,15 @@ Player.prototype.moveRight = function() {
     //need to check sqrs form top and bottom sqrs of player
     let top_contact = this.detectContact(1,0);
     let bot_contact = this.detectContact(1,1);
-    if (top_contact[0] == 'wall' && bot_contact[0] == 'wall') {
+    if (top_contact[0] == 'w' && bot_contact[0] == 'w') {
         this.undraw();
         this.x += SQR;
         this.draw();
     }
-    else if (top_contact[0] == 'door') {
+    else if (top_contact[0] == 'd') {
         player.throughDoor(top_contact[1], top_contact[2]);
     }
-    else if (bot_contact[0] == 'door') {
+    else if (bot_contact[0] == 'd') {
         player.throughDoor(bot_contact[1], bot_contact[2]);
     }
 };
@@ -85,7 +85,7 @@ Player.prototype.detectContact = function (dx,dy) {
     let space = room.getSqr(room_x,room_y);
     
     if (space == 'w') {return ['w'];}
-    else if (space == 'd') {return ['door', room_x, room_y];}
+    else if (space == 'd') {return ['d', room_x, room_y];}
     else {return false;}
 };
 
