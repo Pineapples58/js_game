eb6349e3ef95d8e43aee98f8a1ef138e62b763b3
Michael Counts <mikecounts58@gmail.com>
2018-09-12 14:01:33 -0400

    Update room.js

--- room.js
+++ room.js
@@ -1,10 +1,10 @@
 
 function Room(data) {
     this.name = data.name;
-    this.layout = layout;
+    this.layout = data.layout;
     this.x = (window.innerWidth-this.layout[0].length)/2;
     this.y = (window.innerHeight-this.layout.length)/2;
-    this.color_pallet = color_pallet;
+    this.color_pallet = data.color_pallet;
 }
 
 Room.prototype.getSqr = function(x,y) {
