df9559389202d6637278df7879dd84e4d886cd92
Michael Counts <mikecounts58@gmail.com>
2018-09-17 12:54:46 -0400

    Update room.js

--- room.js
+++ room.js
@@ -1,34 +1,4 @@
 
-function Floor(data) {
-    this.x = room.getX()+data.x;
-    this.y = room.getY()+data.y;
-    this.x_len = data.x_len;
-    this.y_len = data.y_len;
-    this.fill_color = data.fill_color;
-    this.stroke_color = data.stroke_color;
-    this.walkable = data.walkable;
-    this.has_interaction = data.has_interaction;
-} 
-
-Floor.prototype.draw = function () {
-    c.fillStyle = this.fill_color;
-    c.strokeStyle = this.stroke_color;
-    c.fillRect(this.x, this.y, this.x_len, this.y_len);
-    c.strokeRect(this.x, this.y, this.x_len, this.y_len);
-};
-Floor.prototype.constructor = Floor;
-
-function Door(data) {
-    Floor.call(this,data);
-    this.next_room = data.next_room;
-}
-Door.prototype.constructor = Door;
-Door.prototype = Object.create(Floor.prototype);
-Door.prototype.interact = function () {
-    room = new Room(room_data[this.next_room]);
-    room.populateLayout();
-};
-
 function Room(data) {
     this.name = data.name;
     this.x = (window.innerWidth-100)/2;
@@ -36,20 +6,6 @@ function Room(data) {
     this.layout = [];
     this.layout_data = data.layout_data; 
     this.layout_ordering = data.layout_ordering;
-    this.layout_ordering.forEach((order_name) => {
-        this.layout_data[order_name].forEach((obj_data) => {
-            switch (order_name) {
-                case 'floor':
-                    this.layout.push(new Floor(obj_data));
-                    break;
-                case 'wall':
-                    this.layout.push(new Floor(obj_data));
-                    break;
-                case 'door':
-                    this.layout.push(new Door(obj_data));
-            }
-        });       
-    });
 }
 
 Room.prototype.constructor = Room;
@@ -117,3 +73,36 @@ var room_data = {
   },  
     
 };
+
+
+
+function Floor(data) {
+    this.x = room.getX()+data.x;
+    this.y = room.getY()+data.y;
+    this.x_len = data.x_len;
+    this.y_len = data.y_len;
+    this.fill_color = data.fill_color;
+    this.stroke_color = data.stroke_color;
+    this.walkable = data.walkable;
+    this.has_interaction = data.has_interaction;
+} 
+
+Floor.prototype.draw = function () {
+    c.fillStyle = this.fill_color;
+    c.strokeStyle = this.stroke_color;
+    c.fillRect(this.x, this.y, this.x_len, this.y_len);
+    c.strokeRect(this.x, this.y, this.x_len, this.y_len);
+};
+Floor.prototype.constructor = Floor;
+
+function Door(data) {
+    Floor.call(this,data);
+    this.next_room = data.next_room;
+}
+Door.prototype.constructor = Door;
+Door.prototype = Object.create(Floor.prototype);
+Door.prototype.interact = function () {
+    room = new Room(room_data[this.next_room]);
+    room.populateLayout();
+};
+
