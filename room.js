75e75192befcf9dbe13a94222f7bd966167c9714
Michael Counts <mikecounts58@gmail.com>
2018-09-18 12:27:24 -0400

    Update room.js

--- room.js
+++ room.js
@@ -63,7 +63,7 @@ var room_data = {
                          {x:20,y:0,x_len:5,y_len:30,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
                          {x:20,y:40,x_len:5,y_len:260,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
                          {x:20,y:300,x_len:50,y_len:5,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
-                         //
+                         /*
                          {x:250,y:350,x_len:5,y_len:130,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
                          {x:400,y:20,x_len:100,y_len:5,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
                          {x:260,y:170,x_len:5,y_len:80,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
@@ -71,8 +71,7 @@ var room_data = {
                          {x:310,y:80,x_len:50,y_len:5,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
                          {x:210,y:480,x_len:50,y_len:5,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
                          {x:40,y:400,x_len:5,y_len:70,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
-                         {x:0,y:330,x_len:150,y_len:50,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
-                         /*
+                         {x:0,y:330,x_len:150,y_len:5,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
                          {x:20,y:0,x_len:5,y_len:30,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
                          {x:20,y:0,x_len:5,y_len:30,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
                          {x:20,y:0,x_len:5,y_len:30,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
