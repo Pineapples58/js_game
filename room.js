f7cd142363b90597ab9f144e3d4e0cdbd9f085e7
Michael Counts <mikecounts58@gmail.com>
2018-09-18 13:32:46 -0400

    Update room.js

--- room.js
+++ room.js
@@ -79,9 +79,10 @@ var room_data = {
                          {x:20,y:0,x_len:5,y_len:30,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},*/
                         ],
                    door:[{x:480,y:480,x_len:10,y_len:10,fill_color:'pink',stroke_color:'pink',walkable:true, has_interaction:true, next_room:'First_Area'}],
-                   crate:[{x:50,y:50,x_len:5,y_len:5,fill_color:'brown',stroke_color:'brown',walkable:false, has_interaction:true, inventory:['torch','sword']}]
+                   crate:[{x:50,y:50,x_len:5,y_len:5,fill_color:'yellow',stroke_color:'yellow',walkable:false, has_interaction:true, inventory:['torch','sword']}],
+                   enemy:[{c:2}],
                   },
-    layout_ordering : ['floor','wall', 'door','crate']
+    layout_ordering : ['floor','wall', 'door','crate','enemy']
   },
   First_Area : {
     name : 'First_Area',
