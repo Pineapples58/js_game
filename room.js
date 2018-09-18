692c56c07833bc3144a020d4c5189ab04ee38527
Michael Counts <mikecounts58@gmail.com>
2018-09-18 12:14:37 -0400

    Update room.js

--- room.js
+++ room.js
@@ -59,7 +59,10 @@ var room_data = {
                    wall:[{x:0,y:0,x_len:500,y_len:5,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
                          {x:0,y:0,x_len:5,y_len:500,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
                          {x:495,y:0,x_len:5,y_len:500,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
-                         {x:0,y:495,x_len:500,y_len:5,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false}],
+                         {x:0,y:495,x_len:500,y_len:5,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
+                         {x:20,y:0,x_len:5,y_len:300,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
+                         {x:20,y:300,x_len:50,y_len:5,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
+                        ],
                    door:[{x:480,y:480,x_len:10,y_len:10,fill_color:'pink',stroke_color:'pink',walkable:true, has_interaction:true, next_room:'First_Area'}],
                    crate:[{x:50,y:50,x_len:5,y_len:5,fill_color:'brown',stroke_color:'brown',walkable:false, has_interaction:true, inventory:['torch','sword']}]
                   },
@@ -72,7 +75,8 @@ var room_data = {
                    wall:[{x:0,y:0,x_len:500,y_len:5,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
                          {x:0,y:0,x_len:5,y_len:500,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
                          {x:495,y:0,x_len:5,y_len:500,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
-                         {x:0,y:495,x_len:500,y_len:5,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false}],
+                         {x:0,y:495,x_len:500,y_len:5,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
+                        ],
                    door:[{x:100,y:480,x_len:10,y_len:10,fill_color:'pink',stroke_color:'pink',walkable:true, has_interaction:true, next_room:'First_Area'}],
                   },
     layout_ordering : ['floor','wall', 'door']
