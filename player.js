886797a05ee8213a403ab247d00c5e9f6abc69be
Michael Counts <mikecounts58@gmail.com>
2018-09-18 14:48:11 -0400

    Update player.js

--- player.js
+++ player.js
@@ -63,10 +63,8 @@ Player.prototype.detectContact = function (dx, dy) {
             continue;
         }
         else {        
-            if (room.layout[i].constructor == Enemy){console.log(room.layout[i]);}
-
             if ((Math.max((this.x+dx),room.layout[i].x) < Math.min((this.x+this.width+dx),(room.layout[i].x+room.layout[i].x_len))) && (Math.max((this.y+dy),room.layout[i].y) < Math.min((this.y+this.height+dy),(room.layout[i].y+room.layout[i].y_len)))) {
-                //if room.layout[i].interaction 
+                if (room.layout[i].constructor == Enemy){console.log(room.layout[i]);}
                 return room.layout[i];
             }
         }
