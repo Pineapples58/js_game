1f31f0709f7c979ecc565f8c35001166fa799adf
Michael Counts <mikecounts58@gmail.com>
2018-09-18 12:48:24 -0400

    Create enemy.js

---
+++ enemy.js
@@ -0,0 +1,20 @@
+function Enemy() {
+    this.x = 50;
+    this.y = 50;
+    this.speed = 2;
+    this.health = 5;
+    this.damage = 1;
+    this.color = 'red';
+    this.alive = true;
+}
+
+Enemy.prototype.beenHit = function (dmg) {
+    if (dmg >= this.health) {
+       this.alive = false;
+       console.log('dead');
+    }
+    else {
+       this.health -= dmg;
+       console.log('hit');
+    }
+};
