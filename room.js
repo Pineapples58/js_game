8e2b146299797d4baac73373e75d4af1e081f551
Michael Counts <mikecounts58@gmail.com>
2018-09-18 15:57:09 -0400

    Update room.js

--- room.js
+++ room.js
@@ -63,8 +63,8 @@ var room_data = {
                          {x:0,y:0,x_len:5,y_len:500,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
                          {x:495,y:0,x_len:5,y_len:500,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
                          {x:0,y:495,x_len:500,y_len:5,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
-                         /*
-                         {x:20,y:0,x_len:5,y_len:30,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
+                         
+                         {x:0,y:50,x_len:100,y_len:5,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
                          {x:20,y:40,x_len:5,y_len:260,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
                          {x:20,y:300,x_len:50,y_len:5,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
                          
@@ -79,7 +79,7 @@ var room_data = {
                          {x:20,y:0,x_len:5,y_len:30,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
                          {x:20,y:0,x_len:5,y_len:30,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
                          {x:20,y:0,x_len:5,y_len:30,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
-                         {x:20,y:0,x_len:5,y_len:30,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},*/
+                         {x:20,y:0,x_len:5,y_len:30,fill_color:'black',stroke_color:'black',walkable:false, has_interaction:false},
                         ],
                    door:[{x:480,y:480,x_len:10,y_len:10,fill_color:'pink',stroke_color:'pink',walkable:true, has_interaction:true, next_room:'First_Area'}],
                    crate:[{x:50,y:50,x_len:5,y_len:5,fill_color:'yellow',stroke_color:'yellow',walkable:false, has_interaction:true, inventory:['torch','sword']}],
